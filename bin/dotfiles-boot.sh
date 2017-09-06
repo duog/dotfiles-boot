72514a1b29e8ffeca06722d727f1bd7d4c5ec08c
Douglas Wilson <douglas.wilson@gmail.com>
2017-09-07 11:18:06 +1200

    ignore untracked

--- bin/dotfiles-boot.sh
+++ bin/dotfiles-boot.sh
@@ -2,6 +2,7 @@
 
 pushd ~
 git init
+git config --local status.ignoreUntracked on
 git remote add origin https://github.com/duog/dotfiles
 git pull origin master
 git submodule update --init --recursive
