2b07e7fcc956580cec6a4e7793d7ddeb9c3a0b63
Douglas Wilson <douglas.wilson@gmail.com>
2017-09-07 11:28:04 +1200

    Fiddling with init

--- bin/dotfiles-boot.sh
+++ bin/dotfiles-boot.sh
@@ -4,7 +4,7 @@ pushd ~
 git init
 git config --local status.ignoreUntracked on
 git remote add origin https://github.com/duog/dotfiles
-git pull origin master
+git checkout -f -b origin/master
 git submodule update --init --recursive
 
 sudo usermod -s /bin/zsh ${USER}
