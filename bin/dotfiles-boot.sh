d6bfacfd53de0d77d67d35d75ab833ba87d3d2e6
Douglas Wilson <douglas.wilson@gmail.com>
2018-01-09 13:26:54 +1300

    Add some updating of settings.
    Non portable, and should definitely be in different scripts

--- bin/dotfiles-boot.sh
+++ bin/dotfiles-boot.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# This is rough, these should be separated into different commands
+
+# checkout dotfiles repo into ~
 pushd ~
 git init
 git config --local status.showUntrackedFiles no
@@ -8,6 +11,17 @@ git fetch origin
 git checkout -f master
 git submodule update --init --recursive
 
+# Set shell
 sudo usermod -s /bin/zsh ${USER}
+
+# Add ppas
+sudo add-apt-repository ppa:ubuntu-mozilla-daily/ppa
+
+# Set default applications
+xdg-settings set default-web-browser firefox-trunk.desktop
+# might need to update-alternatives x-www-browser
+
+# turn off blinking cursor in gnome-terminal
+gsettings set org.gnome.desktop.interface cursor-blink false
 
 popd
